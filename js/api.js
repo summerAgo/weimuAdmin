c433b00533ead3f095edddb4f21b15b308977600
luobobaicaijiazu <1216331654@qq.com>
2018-11-02 20:17:28 +0800

    luojie

--- js/api.js
+++ js/api.js
@@ -22,3 +22,25 @@ var outTreasury = pathUrl + "order/outtreasury/";//出库
 // 用户管理
 var userList = pathUrl + "sys/list";//用户列表
 var userAdd = pathUrl + "sys/add";//添加用户
+var userUpdate = pathUrl + "sys/update";//修改用户
+var userDel = pathUrl + "sys/deleteMany";//删除用户
+var userPas = pathUrl + "sys/resetPassword";//重置密码
+var userExit = pathUrl + "sys/exit";//退出登陆
+//角色
+var roleList = pathUrl + "sys/role/list";//角色列表
+var roleAdd = pathUrl + "sys/role/add";//添加角色
+var roleUpdate = pathUrl + "sys/role/update";//修改角色
+var roleDel = pathUrl + "sys/role/deleteMany";//删除角色
+var roleAuthority = pathUrl + "sys/role/setAuthority";//设置角色对应菜单(roleid 角色id        menuId[]菜单数组)
+//菜单
+var menuList = pathUrl + "sys/menu/list";//菜单列表
+var menuAdd = pathUrl + "sys/menu/add";//添加菜单
+var menuUpdate = pathUrl + "sys/menu/update";//修改菜单
+var menuDel = pathUrl + "sys/menu/deleteMany";//删除菜单
+var menuAuthority = pathUrl + "sys/menu/setAuthority";//设置菜单对应按钮(menuid 菜单id        butId[]按钮数组)
+var menuAll = pathUrl + "sys/menu/droplist";//查询所有
+//按钮
+var buttonList = pathUrl + "sys/button/list";//按钮列表
+var buttonAdd = pathUrl + "sys/button/add";//添加按钮
+var buttonUpdate = pathUrl + "sys/button/update";//修改按钮
+var buttonDel = pathUrl + "sys/button/deleteMany";//删除按钮
