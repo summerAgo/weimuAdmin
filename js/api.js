05dfa85b2520d8e489732c7d51189de3b4449921
luobobaicaijiazu <1216331654@qq.com>
2018-11-07 18:13:27 +0800

    luojie

--- js/api.js
+++ js/api.js
@@ -110,3 +110,4 @@ var buttonAllList = pathUrl + "sys/button/dropdown";//查询按钮所有的
 
 var rolegetAuthority = pathUrl + "sys/role/getAuthority";//查询角色对应菜单(roleid 角色id)]
 var menugetAuthority = pathUrl + "sys/menu/getAuthority";//查询菜单对应按钮(menuid 菜单id)
+var getAuthorityAll = pathUrl + "/sys/role/getAuthorityAll";//根据角色id查询所有的按钮权限(roleid=2)
