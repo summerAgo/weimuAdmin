912afccd4cdde9f5a997dc791ca95666e695c457
sky <447644896@qq.com>
2018-11-08 18:23:37 +0800

    lss

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 var localport = ":8000";//本地端口
 var localhost = "http://192.168.10.101" + localport;//本地
-var pathUrl = "http://192.168.10.201:80/storage-web/"; //?username=admin&password=123456
+var pathUrl = "http://192.168.10.201:8080/storage-web/"; //?username=admin&password=123456
 var loginUrl = pathUrl + "sys/login"; //登录
 //基础货物公共信息
 var commonBaseUrl = pathUrl + "c/goodsbasics/list"; //查询货物
