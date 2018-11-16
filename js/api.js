876762b438a74b6f5a8d4460a23664418cbd1edc
summerAgo <summerago@vip.qq.com>
2018-11-16 11:12:05 +0800

    dean

--- js/api.js
+++ js/api.js
@@ -1,8 +1,6 @@
 var localport = ":8000";//本地端口
-var localhost = "http://192.168.10.201" + localport;//本地
-var pathUrl = "http://192.168.10.201:80/storage-web/"; //?username=admin&password=123456
 var localhost = "http://192.168.10.216" + localport;//本地
-var pathUrl = "http://192.168.10.201:8080/storage-web/"; //?username=admin&password=123456
+var pathUrl = "http://192.168.10.201:80/storage-web/"; //?username=admin&password=123456
 
 var loginUrl = pathUrl + "sys/login"; //登录
 
