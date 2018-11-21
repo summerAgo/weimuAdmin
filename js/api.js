42a33813ed20e51a415cd7ca8fe1d7e0f0c7fc67
xp <459264080@qq.com>
2018-11-21 19:57:49 +0800

    xp

--- js/api.js
+++ js/api.js
@@ -1,11 +1,3 @@
-// var localport = ":8080";//服务器端口
-// var localhost = "http://119.23.68.13" + localport;//本地
-var pathUrl = "http://119.23.68.13/storage-web/"; //?username=admin&password=123456
-
-var localport = ":8000";//本地端口
-var localhost = "http://192.168.10.110" + localport;//本地
-// var pathUrl = "http://192.168.10.201:80/storage-web/"; //?username=admin&password=123456
-
 var loginUrl = pathUrl + "sys/login"; //登录
 var loginPageUrl = localhost + "/view/login/login.html"; //filter拦截到登录页面
 
