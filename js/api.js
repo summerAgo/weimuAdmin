a85f9a8b152333705c148d19aaadd00ab2b62ee5
sky <>
2018-12-04 17:17:31 +0800

    lss

--- js/api.js
+++ js/api.js
@@ -200,6 +200,7 @@ var isname_position = pathUrl +"basics/position/isName";        //库位验证  
 var isname_institution = pathUrl +"basics/institution/isName";  //类型信息  utype:0员工1仓库主2供应商3基础数据 ,name
 var isname_info = pathUrl +"basics/info/isName";                //货物信息      instid,name
 var isname_depot = pathUrl +"basics/depot/isName";              //仓库验证      ownerid,name
+var isname_bitdepot = pathUrl +"basics/bitdepot/isName";        //大仓库验证    name
 var isname_user = pathUrl +"sys/isName";                        //员工验证      instid,
 var isname_role = pathUrl +"sys/role/isName";                   //角色验证      rolename
 var isname_owner = pathUrl +"user/owner/isName";                //仓库主验证    instid,name
