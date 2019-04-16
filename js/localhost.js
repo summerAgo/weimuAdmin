4e35003d7445b69d25fd986f81286b1c5d2ca63d
chao <38246974+125002850@users.noreply.github.com>
2019-04-16 16:10:09 +0800

    chao 4/16

--- js/localhost.js
+++ js/localhost.js
@@ -4,4 +4,5 @@
 
 var localport = ":8000";//本地端口
 var localhost = "http://192.168.10.211" + localport;//本地
-var pathUrl = "http://192.168.10.253:80/storage-web/";
+// var pathUrl = "http://192.168.10.253:80/storage-web/"; //谢平接口
+var pathUrl = "http://192.168.10.211:8080/storage-web/"; 
