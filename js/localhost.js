c5a800e336cacc238ca58b2c61f721d0a25c1f88
summerAgo <44191411+summerAgo@users.noreply.github.com>
2019-04-22 14:21:05 +0800

    dean

--- js/localhost.js
+++ js/localhost.js
@@ -2,7 +2,7 @@
 // var localhost = "http://t.weimumall.com" + localport;//本地
 var pathUrl = "http://t.weimumall.com/storage-web/";       //http://vmapi.weimumall.com
 
-var localport = ":8000";//本地端口
-var localhost = "http://192.168.10.211" + localport;//本地
+var localport = ":5500";//本地端口
+var localhost = "http://192.168.10.251" + localport;//本地
 // var pathUrl = "http://192.168.10.253:80/storage-web/"; //谢平接口
 //var pathUrl = "http://192.168.10.211:8080/storage-web/"; 
